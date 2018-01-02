bee00a88fab7b096d881901dc1cea10eb4ff7437
GayashanK <kgdharmasiri@gmail.com>
2018-01-02 17:53:40 +0530

    develop async/await promises

--- controllers/users.js
+++ controllers/users.js
@@ -13,26 +13,28 @@ module.exports = {
     //     });
     // },
 
-    // Use of promises
-    index: (req, res, next) => {
-        User.find({})
-            .then(users => {
-                res.status(200).json(users);
-            })
-            .catch(err => {
-                next(err);
-            });
+    // // Use of promises
+    // index: (req, res, next) => {
+    //     User.find({})
+    //         .then(users => {
+    //             res.status(200).json(users);
+    //         })
+    //         .catch(err => {
+    //             next(err);
+    //         });
+    // },
+
+    // Use of async,await
+
+    index: async (req, res, next) => {
+        const user = await User.find({});
+        res.status(200).json(users);
     },
 
-    newUser: (req, res, next) => {
+    newUser: async (req, res, next) => {
         const newUser = new User(req.body);
-        newUser.save()
-            .then(user => {
-                res.status(201).json(user);
-            })
-            .catch(err => {
-                next(err);
-            });
+        const user = await newUser.save();
+        res.status(201).json(user);
     }
 };
 
